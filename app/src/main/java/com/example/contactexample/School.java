7eb7706342ade2fc560e6d1ce87deebfe9d596e9
Mabel <mahurd@email.sc.edu>
2025-06-09 16:50:42 -0400

    Updated school to include a nationality as well as an ID

--- app/src/main/java/com/example/contactexample/School.java
+++ app/src/main/java/com/example/contactexample/School.java
@@ -4,9 +4,36 @@ import java.io.Serializable;
 
 public class School implements Serializable {
     private final String name;
+    private final Nationality location;
+    private long Id;
 
-    public School(String name) {
+    public School(String name, Nationality location) {
+        if(name == null) {
+            throw new IllegalArgumentException("School name cannot be null");
+        }
+        if(location == null) {
+            throw new IllegalArgumentException("School location cannot be null");
+        }
+
         this.name = name;
+        this.location = location;
+    }
+
+    public School(String name, Nationality location, long Id) {
+        this(name, location);
+        this.Id = Id;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        if(this.Id == -1) {
+            this.Id = Id;
+        } else {
+            throw new IllegalStateException("Id already set");
+        }
     }
 
     public String getName() {
@@ -16,5 +43,9 @@ public class School implements Serializable {
     @Override
     public String toString() {
         return name;
+    }
+
+    public Nationality getLocation() {
+        return location;
     }
 }
